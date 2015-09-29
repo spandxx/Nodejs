511b4ddbd29dd5bc4627c0f2a4c6e72eba470446
spandxx <matthieu.football@hotmail.fr>
2015-09-29 18:51:16 +0200

    exercice

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
 var app = express();
+var choices = require('./choices.json');
+console.log(choices[0].left.name);
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -9,25 +11,53 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-var votes = {
-	'lamasticot': 0,
-	'troll': 0
+
+var votes = null;
+var start = function (i, socket) {
+  votes =  choices[i];
+  votes.left.total = 0;
+  votes.right.total = 0;
+  io.sockets.emit('votes', votes);
 };
 
 
-io.on('connection', function(socket){
-	socket.emit('message', 'welcome');
+setInterval(function(){
+	var rand = Math.floor(Math.random() * choices.length);
+	start(rand);
 
-	console.log('user connected');
-	socket.on('choice', function(what){
-	console.log('chosen:' + what);
-		if (what === 'lamasticot'){
-			votes['lamasticot']++;
-		}else{
-			votes['troll']++;
-		}
-		console.log(votes);
-	});
+	/*var time;
+	for (time=3; time!='-1' ; time--){
+	console.log(time);
+	start(time);
+	}*/
+
+}, 4000);
+start(0);
+
+io.on('connection', function(socket){
+  // send all data
+  socket.emit('votes', votes);
+
+  socket.on('choice', function(what){
+    console.log(what);
+    if (what === 'left') {
+      votes.left.total++;
+    } else {
+      votes.right.total++;
+    }
+    io.sockets.emit('total', votes);
+  });
+
+  socket.on('unchoice', function(what){
+    console.log(what);
+    if (what === 'left') {
+      votes.left.total--;
+    } else {
+      votes.right.total--;
+    }
+    io.sockets.emit('total', votes);
+  });
+
 });
 
 
