93498fed08b8bc3fadf56f5cf6e9605116664eda
spandxx <matthieu.football@hotmail.fr>
2015-09-29 15:27:20 +0200

    projet

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var io = require('socket.io');
 
 var express = require('express');
 var app = express();
 
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
+
+var votes = {
+	'lamasticot': 0,
+	'troll': 0
+};
+
+
+io.on('connection', function(socket){
+	socket.emit('message', 'welcome');
+
+	console.log('user connected');
+	socket.on('choice', function(what){
+	console.log('chosen:' + what);
+		if (what === 'lamasticot'){
+			votes['lamasticot']++;
+		}else{
+			votes['troll']++;
+		}
+		console.log(votes);
+	});
+});
+
 
 http.listen(3000, function(){
 	console.log('listeninggs on *:3000');
